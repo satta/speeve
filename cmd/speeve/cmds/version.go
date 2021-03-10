e8980ebb8025efde35a42c70d6eb2b81e6b6f5ff
Sascha Steinbiss <satta@debian.org>
2021-03-10 15:28:46 +0100

    add version subcommand

---
+++ cmd/speeve/cmds/version.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	version = "0.1.0"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show speeve version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
